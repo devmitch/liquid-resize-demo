148522ba6374468a54377dd9c75c1754a7979e6f
Mitchell Henry <mitchell.henry334@gmail.com>
2023-01-25 12:19:36 +1100

    Rendering textures using PBO implemented

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::sync::{Arc, Mutex};
 
 use eframe::egui;
 use egui_glow::CallbackFn;
-use glow::{NativeBuffer, NativeShader, NativeTexture, NativeVertexArray};
+use glow::{NativeShader, NativeTexture, NativeVertexArray};
 use image::DynamicImage;
 use rfd::FileDialog;
 
@@ -20,10 +20,6 @@ fn main() {
     )
 }
 
-// I probably wanna do this and write to a framebuffer bound to a texture
-// https://stackoverflow.com/questions/3887636/how-to-manipulate-texture-content-on-the-fly/10702468#10702468
-// https://github.com/emilk/egui/blob/master/examples/custom_3d_glow/src/main.rs
-
 // App state goes here
 #[derive(Default)]
 struct LiquidResizeApp {
@@ -31,8 +27,6 @@ struct LiquidResizeApp {
     canvas: Option<Arc<Mutex<GlowImageCanvas>>>,
     pixel_data: Option<DynamicImage>,
 }
-
-// If I never use this, remove and simply use LiquidResizeApp::default() in main fn
 
 impl LiquidResizeApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
@@ -45,8 +39,9 @@ impl LiquidResizeApp {
     fn draw_image(&self, ui: &mut egui::Ui) {
         if let (Some(canvas), Some(image)) = (&self.canvas, &self.pixel_data) {
             let (rect, _) = ui.allocate_exact_size(
+                // can scale width and height down if image is too big
                 egui::Vec2::new(image.width() as f32, image.height() as f32),
-                egui::Sense::click(),
+                egui::Sense::drag(),
             );
 
             let canvas = canvas.clone();
@@ -70,22 +65,27 @@ impl eframe::App for LiquidResizeApp {
             ui.heading("Hello");
             if ui.button("Open image").clicked() {
                 if let Some(path) = FileDialog::new().pick_file() {
-                    let path = path.display().to_string();
                     self.pixel_data = match image::open(&path) {
-                        Ok(image) => {
-                            self.picked_path = Some(path);
+                        Ok(mut image) => {
+                            image = image.flipv();
+                            self.picked_path = Some(path.display().to_string());
                             let gl = frame.gl().expect("eframe not running with glow backend");
                             let new_canvas = Arc::new(Mutex::new(GlowImageCanvas::new(
                                 gl,
                                 image.width(),
                                 image.height(),
                                 image.as_bytes(),
+                                image.color().has_alpha(),
                             )));
                             self.canvas = Some(new_canvas);
                             Some(image)
                         }
                         Err(err) => {
-                            self.picked_path = Some(err.to_string());
+                            let file_name = match path.file_name() {
+                                Some(name) => name.to_str().unwrap_or(".."),
+                                None => "..",
+                            };
+                            self.picked_path = Some(format!("({}) {}", file_name, err.to_string()));
                             None
                         }
                     }
@@ -99,7 +99,7 @@ impl eframe::App for LiquidResizeApp {
                 });
             }
 
-            if let Some(pixel_data) = &self.pixel_data {
+            if self.pixel_data.is_some() {
                 egui::Frame::canvas(ui.style()).show(ui, |ui| {
                     self.draw_image(ui);
                 });
@@ -109,20 +109,21 @@ impl eframe::App for LiquidResizeApp {
     }
 }
 
-fn load_image_from_path(path: &std::path::Path) -> Result<DynamicImage, image::ImageError> {
-    image::open(path)
-}
-
 struct GlowImageCanvas {
     program: glow::Program,
     vao: NativeVertexArray,
-    pbo: NativeBuffer,
     tex: NativeTexture,
 }
 
 impl GlowImageCanvas {
-    fn new(gl: &glow::Context, width: u32, height: u32, pixel_data: &[u8]) -> Self {
-        use glow::HasContext as ctx; // ????
+    fn new(
+        gl: &glow::Context,
+        width: u32,
+        height: u32,
+        pixel_data: &[u8],
+        has_alpha: bool,
+    ) -> Self {
+        use glow::HasContext as _;
         let shader_version = if cfg!(target_arch = "wasm32") {
             "#version 300 es"
         } else {
@@ -132,14 +133,22 @@ impl GlowImageCanvas {
             let program = gl.create_program().expect("Failed to create program");
             let (vertex_shader_source, fragment_shader_source) = (
                 r#"
-                    in vec2 vVertex;
+                    const vec2 verts[6] = vec2[6](
+                        vec2(-1.0, -1.0),
+                        vec2(1.0, -1.0),
+                        vec2(1.0, 1.0),
+                        vec2(-1.0, 1.0),
+                        vec2(-1.0, -1.0),
+                        vec2(1.0, 1.0)
+                    );
                     out vec2 vUV;
                     void main() {
-                        gl_Position = vec4(vVertex*2.0-1,0,1);
-                        vUV = vVertex;
+                        gl_Position = vec4(verts[gl_VertexID], 0.0, 1.0);
+                        vUV = (verts[gl_VertexID] + 1) / 2;
                     }
                 "#,
                 r#"
+                    precision mediump float;
                     in vec2 vUV;
                     out vec4 vFragColor;
                     uniform sampler2D textureMap;
@@ -153,7 +162,6 @@ impl GlowImageCanvas {
                 (glow::VERTEX_SHADER, vertex_shader_source),
                 (glow::FRAGMENT_SHADER, fragment_shader_source),
             ];
-
             let shaders: Vec<NativeShader> = shader_sources
                 .iter()
                 .map(|(shader_type, shader_source)| {
@@ -177,39 +185,12 @@ impl GlowImageCanvas {
                 panic!("{}", gl.get_program_info_log(program));
             }
 
-            // ???
             for shader in shaders {
                 gl.detach_shader(program, shader);
                 gl.delete_shader(shader);
             }
 
             let vao = gl.create_vertex_array().expect("Failed to create VAO");
-            let vbo = gl.create_buffer().expect("Failed to create VBO");
-            let ebo = gl.create_buffer().expect("Failed to create EBO");
-            gl.bind_vertex_array(Some(vao));
-
-            gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
-            // could maybe be const variable?
-            let vertices: [u8; 8] = [0, 0, 1, 0, 1, 1, 0, 1];
-            gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, &vertices, glow::STATIC_DRAW);
-
-            gl.bind_buffer(glow::ELEMENT_ARRAY_BUFFER, Some(ebo));
-            let indices: [u8; 6] = [0, 1, 3, 1, 2, 3];
-            gl.buffer_data_u8_slice(glow::ELEMENT_ARRAY_BUFFER, &indices, glow::STATIC_DRAW);
-
-            gl.vertex_attrib_pointer_f32(
-                0,
-                2,
-                glow::FLOAT,
-                false,
-                (2 * std::mem::size_of::<f32>()) as i32,
-                0,
-            );
-            gl.enable_vertex_attrib_array(0);
-
-            //cleanup
-            gl.bind_buffer(glow::ARRAY_BUFFER, None);
-            gl.bind_vertex_array(None);
 
             // texture setup
             let tex = gl.create_texture().expect("Failed to create texture");
@@ -226,61 +207,28 @@ impl GlowImageCanvas {
                 glow::LINEAR as i32,
             );
 
-            // gl.tex_parameter_i32(glow::TEXTURE_2D, glow::TEXTURE_WRAP_S, glow::REPEAT as i32);
-            // gl.tex_parameter_i32(glow::TEXTURE_2D, glow::TEXTURE_WRAP_T, glow::REPEAT as i32);
-
             let pbo = gl.create_buffer().expect("Failed to create PBO");
             gl.bind_buffer(glow::PIXEL_UNPACK_BUFFER, Some(pbo));
-
-            println!("before!");
-            // maybe should be 3?
-            const CHANNELS: u32 = 4;
-            println!("width = {width}, height = {height}");
-            // gl.buffer_data_size(
-            //     glow::PIXEL_UNPACK_BUFFER,
-            //     (width * height * CHANNELS) as i32,
-            //     glow::STREAM_DRAW,
-            // );
-            // let mapped_buffer = gl.map_buffer_range(
-            //     glow::PIXEL_UNPACK_BUFFER,
-            //     0,
-            //     (width * height * CHANNELS) as i32,
-            //     glow::WRITE_ONLY,
-            // );
-            // println!("before!");
-            // std::ptr::copy_nonoverlapping(
-            //     pixel_data.as_ptr(),
-            //     mapped_buffer,
-            //     (width * height * CHANNELS) as usize,
-            // );
             gl.buffer_data_u8_slice(glow::PIXEL_UNPACK_BUFFER, pixel_data, glow::STREAM_DRAW);
-            println!("after!");
 
-            //gl.bind_buffer(glow::PIXEL_UNPACK_BUFFER, Some(pbo));
-            //gl.unmap_buffer(glow::PIXEL_UNPACK_BUFFER);
+            let format = if has_alpha { glow::RGBA } else { glow::RGB };
             gl.tex_image_2d(
                 glow::TEXTURE_2D,
                 0,
-                glow::RGBA as i32,
+                format as i32,
                 width as i32,
                 height as i32,
                 0,
-                glow::RGBA,
+                format,
                 glow::UNSIGNED_BYTE,
                 None,
             );
             gl.generate_mipmap(glow::TEXTURE_2D);
 
-            Self {
-                program,
-                vao,
-                pbo,
-                tex,
-            }
+            Self { program, vao, tex }
         }
     }
 
-    // we might not need to use ColorImage, and instead use raw pixel data from image io read
     fn paint(&self, gl: &glow::Context) {
         use glow::HasContext as _;
         unsafe {
@@ -291,7 +239,7 @@ impl GlowImageCanvas {
                 gl.get_uniform_location(self.program, "textureMap").as_ref(),
                 0,
             );
-            gl.draw_elements(glow::TRIANGLES, 6, glow::UNSIGNED_INT, 0);
+            gl.draw_arrays(glow::TRIANGLES, 0, 6);
         }
     }
 }
